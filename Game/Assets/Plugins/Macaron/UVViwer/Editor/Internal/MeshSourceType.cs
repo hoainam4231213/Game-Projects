13a20263f54c047d042606f5c5c833749b37e2c2
hoainam4231213 <114986335+hoainam4231213@users.noreply.github.com>
2023-04-14 20:01:20 +0700

    Update

--- Game/Assets/Plugins/Macaron/UVViwer/Editor/Internal/MeshSourceType.cs
+++
@@ -1,8 +0,0 @@
-namespace Macaron.UVViewer.Editor.Internal
-{
-    internal enum MeshSourceType
-    {
-        SelectedObject,
-        Custom
-    }
-}
--- README.md
+++
@@ -1,4 +0,0 @@
-Chạy File bằng Unity hub và Open thư mục Game để chạy
-Chạy Boot Scene để chơi game
-click vào icon súng để chuyển sang súng khác
-..
